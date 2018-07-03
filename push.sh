3a0e1ede580831d94cd46bcf25d4240b4a3c5e6d
Jason Kuruzovich <jkuruzovich@gmail.com>
2018-07-03 19:19:44 -0400

    Pushing with script

--- push.sh
+++ push.sh
@@ -1,2 +1,6 @@
-buster generate --domain=http://localhost:2369 --web-url=http://jasonkuruzovich.com 
-buster deploy
+ghost buster
+#buster generate --domain=http://localhost:2369 --web-url=http://jasonkuruzovich.com 
+#buster deploy
+git add -A
+git commit -m "Pushing with script"
+git push
